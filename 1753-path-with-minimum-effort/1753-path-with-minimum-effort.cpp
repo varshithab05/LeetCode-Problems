1aa49390177ffdf84dce244547d97d0b73bb9849
varshithab05 <117563974+varshithab05@users.noreply.github.com>
2023-09-16 21:31:54 +0530

    Time: 116 ms (63.71%), Space: 18 MB (98.21%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,52 +1,62 @@
 class Solution {
 private:
-    bool visited[105][105];
-    int directions_x[4] = {0, 1, -1, 0};
-    int directions_y[4] = {1, 0, 0, -1}; 
-    int numRows, numCols;
+    int effort[105][105];    // Store effort for each cell
+    int dx[4] = {0, 1, -1, 0};  // Changes in x coordinate for each direction
+    int dy[4] = {1, 0, 0, -1};  // Changes in y coordinate for each direction
 public:
-    void dfs(int x, int y, int limitEffort, vector<vector<int>>& heights){
-        if (visited[x][y])
-            return;
-        visited[x][y] = true;
+    int dijkstra(vector<vector<int>>& heights){
+        int rows = heights.size();
+        int cols = heights[0].size();
 
-        // Stop if we've reached the bottom-right cell
-        if (x == numRows - 1 && y == numCols - 1)
-            return ;
+        // Priority queue to store {effort, {x, y}}
+        std::priority_queue<std::pair<int, std::pair<int, int>>> pq;
+        pq.push({0, {0, 0}});  // Start from the top-left cell
+        effort[0][0] = 0;  // Initial effort at the starting cell
 
-        // Explore each direction (up, down, left, right)
-        for (int i = 0; i < 4; i++) {
-            int new_x = x + directions_x[i];
-            int new_y = y + directions_y[i];
+        while (!pq.empty()) {
+            auto current = pq.top().second;
+            int cost = -pq.top().first;  // Effort for the current cell
+            pq.pop();
 
-            // Check if the new coordinates are within bounds
-            if (new_x < 0 || new_x >= numRows || new_y < 0 || new_y >= numCols)
+            int x = current.first;
+            int y = current.second;
+
+            // Skip if we've already found a better effort for this cell
+            if (cost > effort[x][y])
                 continue;
-            
-            // Go to next cell if the effort is within limit
-            int newEffort = abs(heights[x][y] - heights[new_x][new_y]);
-            if (newEffort <= limitEffort)
-                dfs(new_x, new_y, limitEffort, heights);
+
+            // Stop if we've reached the bottom-right cell
+            if (x == rows - 1 && y == cols - 1)
+                return cost;
+
+            // Explore each direction (up, down, left, right)
+            for (int i = 0; i < 4; i++) {
+                int new_x = x + dx[i];
+                int new_y = y + dy[i];
+
+                // Check if the new coordinates are within bounds
+                if (new_x < 0 || new_x >= rows || new_y < 0 || new_y >= cols)
+                    continue;
+
+                // Calculate new effort for the neighboring cell
+                int new_effort = std::max(effort[x][y], std::abs(heights[x][y] - heights[new_x][new_y]));
+
+                // Update effort if a lower effort is found for the neighboring cell
+                if (new_effort < effort[new_x][new_y]) {
+                    effort[new_x][new_y] = new_effort;
+                    pq.push({-new_effort, {new_x, new_y}});
+                }
+            }
         }
+        return effort[rows - 1][cols - 1]; 
     }
     int minimumEffortPath(vector<vector<int>>& heights) {
-        numRows = heights.size(); 
-        numCols = heights[0].size();
-
-        // Bound for our binary search
-        int lowerLimit = 0, upperLimit = 1e6;
-
-        while (lowerLimit < upperLimit) {
-            int mid = (upperLimit + lowerLimit) / 2;
-            memset(visited, 0, sizeof visited);
-            dfs(0, 0, mid, heights);
-
-            if (visited[numRows - 1][numCols - 1])
-                upperLimit = mid;
-            else
-                lowerLimit = mid + 1;
+        // Initialize effort for each cell to maximum value
+        for (int i = 0; i < heights.size(); i++) {
+            for (int j = 0; j < heights[i].size(); j++) {
+                effort[i][j] = INT_MAX;
+            }
         }
-
-        return lowerLimit;
+        return dijkstra(heights);  // Find minimum effort using dijkstra
     }
 };
